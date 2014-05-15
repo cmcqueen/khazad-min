9e628bbb011e94609ca5cd17285e1329edc3e504
Craig McQueen <craig.mcqueen@beamcommunications.com>
2014-05-15 10:31:57 +1000

    Tidy up S-box extern symbols.

--- khazad-sbox.h
+++ khazad-sbox.h
@@ -7,15 +7,6 @@
 #include <stdint.h>
 
 
-extern const uint8_t khazad_sbox_table[256u];
-
-
-static inline uint8_t khazad_sbox(uint8_t a)
-{
-    return khazad_sbox_table[a];
-}
-
-
 void khazad_sbox_apply_block(uint8_t p_block[KHAZAD_BLOCK_SIZE]);
 void khazad_sbox_add_round_const(uint8_t p_block[KHAZAD_BLOCK_SIZE], uint_fast8_t round);
 
